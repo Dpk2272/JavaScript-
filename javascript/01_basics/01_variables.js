cb306a7b266eb2fb3893e22c1d14cb10f636248c
Deepak  Chaudhary <117335352+Dpk2272@users.noreply.github.com>
2024-07-18 19:22:08 +0530

    Add files via upload

---
+++ javascript/01_basics/01_variables.js
@@ -0,0 +1,25 @@
+const accountId = 1234
+let accountEmail = "deepak"
+var accountPassword = "2345"
+accountCity = " delhi"
+let accountState 
+
+//accountId =2  not allowed 
+
+accountEmail = "abc@we.com"
+
+
+console.log(accountId);
+
+/*
+prefer not to use var
+because of issue in block scope and funtional scope 
+
+*/
+console.table ([accountId,accountEmail,accountPassword,accountCity])
+
+
+ 
+
+
+
